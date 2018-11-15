c85aa803b623d25abc56c0cf88fb6e6129a8e93d
okaysidd <siddhartha.sehgal94@gmail.com>
2018-11-15 19:31:10 +0530

    Simple twitter bot app created for learning purpose.

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Twitter-bot-okaysidd
+## Introduction
+This is a simple twitter bot that replies to any tweet that mentions <i>@okaysidd2</i> twitter account and has the text <i>#okaysidd</i> in it.
+
+While the functionality of this this bot is limited, and useless for the time being, it was fun creating it and have many new ideas to implement in more bots in the future.
+
+## Resources
+- The Twitter-bot-okaysidd project is written in **Python version 3.6**.
+- Twitter developer account is needed to get the Consumer API keys and the Access tokens. To create a Twitter Developer account, go [here](https://developer.twitter.com/).
+- The tweepy module is used for getting api functionalities from Twitter. To get Tweepy module for yourself, go [here](https://github.com/tweepy/tweepy).
+
+## How to use
+- The <i>my_twitter_bot.py</i> file contains the functions to run the bot.
+While the <i>my_twitter_bot.py</i> contains the Consumer keys and access tokens that I used, I will destroy them, so anyone using the project will have to have their own Twitter developer account and all necessary keys.
+- The <i>last_seen.txt</i> file contains the latest <i>id</i> of the tweet that was read, so that next time the bot reads tweets that were tweeted after that.
+
+##### To install and setup Python 3.6- go [here](about:blank).
---
+++ my_twitter_bot.py
@@ -0,0 +1,45 @@
+import tweepy
+import time
+
+FILE_NAME = 'last_seen.txt'
+print('My twiter bot')
+
+CONSUMER_KEY = 'lsmZwSPdLjkKjF3lAoslYFoKP'
+CONSUMER_SECRET = '22poxBoxPWzfvRBYWaM4uuwC9UuaxFVQinfgM4PPpgvqWrgt44'
+ACCESS_KEY = '1062705651745345537-krviZj55FHDIlmq0MZfyV2kC8g9s2H'
+ACCESS_SECRET = 'N2GOQnZuHS39JPj5hPOmTDIkpPlUGGLWrlj4qtv5T5M3w'
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+api = tweepy.API(auth)
+
+def retrieve_last_seen_id(filename):
+    f_read = open(filename, 'r')
+    last_seen_id = f_read.read()
+    f_read.close()
+    return last_seen_id
+
+def store_last_seen_id(last_seen_id, filename):
+    f_write = open(filename, 'w')
+    f_write.write(str(last_seen_id))
+    f_write.close()
+    return
+
+def reply_to_tweet():
+    # DEV NOTE- use 1062755960848969729 for testing from beginning
+
+    print('Starting to retrieve tweets after the last checked tweet, and replying to those with #okaysidd in them.')
+    last_seen_id = retrieve_last_seen_id(FILE_NAME)
+    mentions = api.mentions_timeline(last_seen_id)
+    for mention in reversed(mentions):
+        last_seen_id = mention.id
+        store_last_seen_id(last_seen_id, FILE_NAME)
+        #print(str(mention.id) + ' -- ' + mention.text)
+        if '#okaysidd' in mention.text.lower():
+            print('Found #okaysidd    responding...\n')
+            api.update_status('@' + mention.user.screen_name + ' test complete', mention.id)
+            print('Responded to tweet -- {}'.format(mention.text))
+
+while 1:
+    reply_to_tweet()
+    time.sleep(60)
